620de1b58e6e1b3b9e462f7a43c8462206d637fd
RileySmithy <134114772+RileySmithy@users.noreply.github.com>
2023-05-20 17:28:12 +1200

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-
-
-@app.route('/web/buttons')
-def buttons():
-    return render_template('home.html')
+    
+    btn_pressed = request.form.get('press')
+    if btn_pressed:
+        return render_template('home.html')
 
 
 if __name__ == '__main__':
